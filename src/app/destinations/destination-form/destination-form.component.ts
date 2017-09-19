086ecfe0b9e43f1edd7eee77cfc466baf0b48b4a
Haico27 <haico.wensink@gmail.com>
2017-09-20 00:20:41 +0200

    added possibility to add images, although in the ugly way (the input consists of a URL in the form

--- src/app/destinations/destination-form/destination-form.component.ts
+++ src/app/destinations/destination-form/destination-form.component.ts
@@ -14,17 +14,22 @@ export class DestinationFormComponent {
   //constructor that injects destinationService into the destinationService-property
   constructor( private destinationService: destinationService) { }
 
-  model = new Destination('', '');
+  model = new Destination('', '', '');
+
+  image: FileList;
 
   submitted = false;
 
   onSubmit(): void {
     console.log("onSubmit() function in destination-form component activated! Data: ", this.model)
-    console.log('this.model', this.model.name, this.model.country);
+    console.log('this.model', this.model.name, this.model.country, this.model.imageUrl);
     this.destinationService.create(this.model)
         .then(destination => console.log(destination))
+  }
 
-
+  onChange(event) {
+    this.image = event.target.files;
+    console.log(this.image)
   }
 
 }
--- src/app/destinations/destination.ts
+++ src/app/destinations/destination.ts
@@ -3,5 +3,6 @@ export class Destination {
   constructor(
     public name: string,
     public country: string,
+    public imageUrl: string,
   ) { }
 }
