b11e28f66ade0a9f402d848ebbd2d6cd2d5509c2
Haico27 <haico.wensink@gmail.com>
2017-09-26 13:51:22 +0200

    change navbar when logged in or not; doesn't reload immediately though

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
+import { AlertService } from './services/alert.service';
+import { Subscription }   from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  subscription: Subscription;
+  currentUser = JSON.parse(localStorage.getItem('currentUser'))
+
+    constructor(
+      private authenticationService: AuthenticationService,
+      private alertService: AlertService
+    ) {
+      console.log("currentUser in Appcomponent: ", this.currentUser)
+
+
+      //when the app refreshes or gets initialized
+      this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
+
+
+    }
+
+    logout() {
+      this.authenticationService.logout();
+      this.alertService.success("You logged out", true)
+    }
+
+
 }
