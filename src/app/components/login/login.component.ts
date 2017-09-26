9e5fadd4c93dfab229f4737eb7bf1c475535fc8a
Haico27 <haico.wensink@gmail.com>
2017-09-26 11:56:38 +0200

    show alert-message after login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,13 +16,13 @@ export class LoginComponent implements OnInit {
   constructor(
     private authenticationService: AuthenticationService,
     private alertService: AlertService,
-    private router: Router,
-
+    private router: Router
   ) { }
 
   model: any = {};
   loading = false;
   returnUrl: string;
+
 
   ngOnInit() {
     //reset loginstatus before logging in
@@ -36,7 +36,25 @@ export class LoginComponent implements OnInit {
     this.loading = true;
     this.authenticationService.login(this.model.email, this.model.password)
         .then(
-          data => { this.router.navigate([this.returnUrl]) }
+
+          data => {
+            if (data !== undefined) {
+              this.alertService.success("Login successful!", true);
+              this.router.navigate([this.returnUrl]);
+            } else {
+              this.alertService.error("Emailaddress or password is incorrect", true);
+              this.loading = false;
+            }
+          }
+          // data => {
+          //   this.alertService.success('login successful!', true);
+          //   this.router.navigate([this.returnUrl]);
+          // },
+          // error => {
+          //   console.log("login failed");
+          //   this.alertService.error('username or password incorrect', true);
+          //   this.loading = false;
+          // }
         )
   }
 
