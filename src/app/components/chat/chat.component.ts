93e3bfcd57d3d73bb22add2259a5eb7a1426aebf
Haico27 <haico.wensink@gmail.com>
2017-10-18 18:42:46 +0200

    chat only visible when logged in

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -5,19 +5,25 @@ import { ChatService } from '../../services/chat.service';
 
 
 
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+currentUser = JSON.parse(localStorage.getItem('currentUser')) || null;
 
-  constructor(private chatService:ChatService) {}
+  constructor(
+    private chatService:ChatService,
+  ) { }
 
-  // gets the currentUser from localStorage
-  sender = JSON.parse(localStorage.getItem('currentUser'));
+
+
+  // sets the sender if currentUser exists
+  sender = this.currentUser ? this.currentUser.firstName : null;
   //creates a new instance of the Message-class, without any content yet
-  message = new Message(this.sender.firstName, '');
+  message = new Message(this.sender, '');
 
   //creates an empty array to store the messages for showing them on the screen
   messages = [];
@@ -29,18 +35,21 @@ export class ChatComponent implements OnInit {
       this.getMessage();
   }
 
+  //sends the message object with sender and text through to the sendMessage function in the chatService
   sendMessage() {
     console.log(this.message.text, this.message.sender)
     console.log(this.message)
     this.chatService.sendMessage(this.message);
     this.message.text = '';
+    console.log("messages array in chat.component: ", this.messages)
 
   }
 
+  //activates the getMessage function in the chatService and pushes every new message in the messages-array
   getMessage() {
     this.chatService.getMessage().subscribe(message =>
-      this.messages.push(message))
-      console.log("messages array in chat.component: ", this.messages)
+      this.messages.push(message)
+    )
   }
 
 
@@ -49,7 +58,7 @@ export class ChatComponent implements OnInit {
 
 export class Message {
   constructor(
-    public sender: { firstName },
+    public sender: string,
     public text: string,
   ) { }
 }
