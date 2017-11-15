2b6f5b60bccd2b3afef1b9fc3a6d9792234ffccd
Haico27 <haico.wensink@gmail.com>
2017-11-15 21:36:09 +0100

    clean up code in destination-form

--- src/app/components/destination-form/destination-form.component.ts
+++ src/app/components/destination-form/destination-form.component.ts
@@ -16,24 +16,13 @@ export class DestinationFormComponent {
 
   model = new Destination('', '', '');
 
-  image: FileList;
-
-  submitted = false;
-
   onSubmit(): void {
-    console.log("onSubmit() function in destination-form component activated! Data: ", this.model)
-    console.log('this.model', this.model.name, this.model.country, this.model.imageUrl);
     this.destinationService.create(this.model as Destination)
         .subscribe(destination => {
-          console.log("in submitform: ", destination)
-          console.log("destinations in submitform: ", this.destinations)
           this.destinations.push(destination)
         })
   }
 
-  onChange(event) {
-    this.image = event.target.files;
-    console.log(this.image)
-  }
+
 
 }
